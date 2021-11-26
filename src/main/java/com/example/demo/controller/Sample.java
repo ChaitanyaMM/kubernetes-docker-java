22a4b19f1128de2f56dcf6b5c678ccefc9a69b54
ChaitanyaMM <krishnachaitu.17@gmail.com>
2021-11-26 21:58:23 +0530

    adding log & networkpolicies

--- src/main/java/com/example/demo/controller/Sample.java
+++ src/main/java/com/example/demo/controller/Sample.java
@@ -1,26 +1,66 @@
 package com.example.demo.controller;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
 @Entity
-@Table(name="sample")
+@Table(name = "sample")
 @Data
 public class Sample {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String gender;
-	
-	
-	
-	
+ 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+	private Date date;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@PrePersist
+	protected void onCreate() {
+		date = new Date();
+	}
 
 }
--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
-import javax.websocket.server.PathParam;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,21 +10,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+ 
 @RestController
 @RequestMapping("/api/sample")
+@Slf4j
 public class SampleController {
+    private static final Logger log = LoggerFactory.getLogger(SampleController.class);
 
+  	
 	@Autowired
 	private SampleRepository sampleRepository;
 
 	@GetMapping("/{id}")
 	public Sample getSample(@PathVariable Long id) {
+        log.info("retrieve data :-> " + id);
 
 		return sampleRepository.findById(id).get();
 	}
 
 	@PostMapping("/")
 	public Sample postSample(@RequestBody Sample sample) {
+        log.info("insert data :-> " + sample);
 
 		return sampleRepository.save(sample);
 	}
